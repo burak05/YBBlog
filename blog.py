427d5ba88b91bf9227a440896a9ae47f0e7f9c79
Burak Keskin <burak.keskin.0505@gmail.com>
2022-10-30 23:35:54 +0300

    add jinja
    add jinja template

--- blog.py
+++ blog.py
@@ -1,7 +1,12 @@
 from distutils.log import debug
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
+
+@app.route("/")
+def index():
+    sayi = 10
+    return render_template("index.html",number=sayi)
 
 if __name__ == "__main__" :
     app.run(debug=True)
