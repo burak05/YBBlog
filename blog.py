729c3c9d774318b6fc4f1c084f0b7d5c22cb9037
Burak Keskin <burak.keskin.0505@gmail.com>
2022-11-01 12:40:34 +0300

    created login form template

--- blog.py
+++ blog.py
@@ -4,6 +4,16 @@ from flask_mysqldb import MySQL
 from wtforms import Form, StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
 
+# Register form
+class RegisterForm(Form):
+    name = StringField("Name Surname",validators=[validators.Length(min=4,max=25)])
+    username = StringField("Username",validators=[validators.Length(min=5,max=35)])
+    email = StringField("Email",validators=[validators.Email(message="Please enter a valid email address!")])
+    password = PasswordField("Password: ",validators=[validators.DataRequired(message="Please enter a password!"),validators.EqualTo(fieldname="confirm",message="Passwords do not match!")])
+    confirm = PasswordField("Confirm Password: ")
+
+
+
 app = Flask(__name__)
 app.config["MYSQL_HOST"]="localhost"
 app.config["MYSQL_USER"]="root"
