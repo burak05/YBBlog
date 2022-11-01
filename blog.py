e709814d3901e27c64140d3c39d48d818434e262
Burak Keskin <burak.keskin.0505@gmail.com>
2022-11-01 12:26:45 +0300

    configured mysql

--- blog.py
+++ blog.py
@@ -5,6 +5,13 @@ from wtforms import Form, StringField,TextAreaField,PasswordField,validators
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
+app.config["MYSQL_HOST"]="localhost"
+app.config["MYSQL_USER"]="root"
+app.config["MYSQL_PASSWORD"]=""
+app.config["MYSQL_DB"]="ybblog"
+app.config["MYSQL_CURSORCLASS"]="DictCursor"
+mysql = MySQL(app)
+
 
 @app.route("/")
 def index():
